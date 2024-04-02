af50c9a1e0a2dafb2a7469c80e5f554b7406dc4d
Max Humeniuk <maxhumenik321@gmail.com>
2024-04-02 21:35:15 +0300

    asdasdasd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,6 @@ import About from '../pages/About';
 import Contact from '../pages/Contact';
 import Locations from '../pages/Locations';
 
-
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [inputValue, setInputValue] = useState('');
@@ -55,10 +54,10 @@ const Navbar = () => {
         {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[35%] h-full border-[#ffd3ba] bg-[#ffd3ba] ease-in-out duration-300' : 'ease-in-out duration-300 fixed left-[-100%]'}>
-        <h1 className='p-5 flex justify-center w-full text-3xl font-bold text-[#824421] m-4'style={{ fontSize: "3rem" }}>Улюблені лапки</h1>
+        <h1 className='p-10 flex justify-center w-full text-3xl font-bold text-[#824421] m-4'style={{ fontSize: "3rem" }}>Улюблені лапки</h1>
         <Link 
           to="/" 
-          className="p-5 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
+          className="p-10 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
           style={{ fontSize: "3rem" }}
           onMouseEnter={() => setIsHoveredHome(true)}
           onMouseLeave={() => setIsHoveredHome(false)}
@@ -74,7 +73,7 @@ const Navbar = () => {
         </Link>
         <Link 
           to="/locations" 
-          className="p-5 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
+          className="p-10 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
           style={{ fontSize: "3rem" }}
           onMouseEnter={() => setIsHoveredLocations(true)}
           onMouseLeave={() => setIsHoveredLocations(false)}
@@ -90,7 +89,7 @@ const Navbar = () => {
         </Link>
         <Link 
           to="/about" 
-          className="p-5 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
+          className="p-10 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
           style={{ fontSize: "3rem" }}
           onMouseEnter={() => setIsHoveredAbout(true)}
           onMouseLeave={() => setIsHoveredAbout(false)}
@@ -106,7 +105,7 @@ const Navbar = () => {
         </Link>
         <Link 
           to="/contact" 
-          className="p-5 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
+          className="p-10 flex justify-center items-center border-gray-600 text-[#733a19] font-bold block hover:text-[#ffd3ba] hover:bg-[#733a19]" 
           style={{ fontSize: "3rem" }}
           onMouseEnter={() => setIsHoveredContact(true)}
           onMouseLeave={() => setIsHoveredContact(false)}
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,33 +1,188 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Імпорт компонента Link
-import restaurantsData from '../assets/restourants.json'; 
-import pawIcon from '../assets/paw_icon.png';
+import React from 'react';
+import Pet from '../assets/kapybara.jpg';
+import arrowImage from '../assets/paw-icon.png'; 
+import Dog from '../assets/dog.jpg';
+import Monkey from '../assets/monkey.jpg';
+import Duck from '../assets/duck.jpg';
+import LazyDog from '../assets/lazy-dog.jpg';
+import Sugar from '../assets/sugar.jpg';
+import Sowa from '../assets/sowa.jpg';
+import CatCafe from '../assets/cat-cafe.jpg';
+import GrandCafe from '../assets/grand-cafe.jpg';
+import BerNarDyn from '../assets/ber-nar-dyn.jpg';
+import Akvarel from '../assets/akvarel.jpg';
+import CraftKumpel from '../assets/craft-kumpel.jpg';
+import LvivManufaktura from '../assets/lviv-manufaktura.jpg';
+import BurhlyviOplisky from '../assets/burhlyvi-oplisky.jpg';
+import Valentino from '../assets/valentino.jpg';
+import MrsGreenwich from '../assets/mrs-greenwich.jpg';
+import NaVirmenskiy from '../assets/na-virmenskiy.jpg';
+import Papi from '../assets/papi.jpg';
+import MoreRyby from '../assets/more-ryby.jpg';
+import EpicCheeseburger from '../assets/epic-cheeseburger.jpg';
+import CukorBlack from '../assets/cukor-black.jpg';
+import Teddy from '../assets/teddy.jpg';
 
-const PetFriendlyPage = ({ nav }) => { // Передаємо змінну nav як пропс
-  const [establishments, setEstablishments] = useState([]);
+const establishments = [
+  
+  {
+    "id": 3,
+    "name": "Dogs like ducks",
+    "description": "🌱 107% веганські хот-доги 🌭📍 вул. Ковжуна, 6, м. Львів 🇺🇦🛒 Веган-маркет 🎁   Instagram: @dogslikeducks",
+    "rating": 4,
+    "image": "duck.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Лінивий пес",
+    "description": "A cozy pet-friendly cafe with a relaxing atmosphere.",
+    "rating": 4.5,
+    "image": "lazy-dog.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Цукор",
+    "description": "A pet-friendly cafe offering sweet treats and drinks.",
+    "rating": 4,
+    "image": "sugar.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Sowa",
+    "description": "A pet-friendly restaurant serving delicious dishes in a cozy setting.",
+    "rating": 4.5,
+    "image": "sowa.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Cat Cafe Lviv",
+    "description": "A pet-friendly cafe where you can enjoy coffee surrounded by friendly cats.",
+    "rating": 5,
+    "image": "cat-cafe.jpg"
+  },
+  {
+    "id": 8,
+    "name": "Grand cafe leopolis",
+    "description": "A pet-friendly cafe with a charming atmosphere and tasty food.",
+    "rating": 4.5,
+    "image": "grand-cafe.jpg"
+  },
+  {
+    "id": 9,
+    "name": "BER NAR DYN Restaurant",
+    "description": "A pet-friendly restaurant offering a wide range of dishes and drinks.",
+    "rating": 4,
+    "image": "ber-nar-dyn.jpg"
+  },
+  {
+    "id": 10,
+    "name": "Ресторан «Акварель»",
+    "description": "A pet-friendly restaurant with a beautiful water-themed decor.",
+    "rating": 4.5,
+    "image": "akvarel.jpg"
+  },
+  {
+    "id": 11,
+    "name": "Craft & Kumpel’ Beer Atelier",
+    "description": "A pet-friendly beer atelier offering craft beers and tasty snacks.",
+    "rating": 4,
+    "image": "craft-kumpel.jpg"
+  },
+  {
+    "id": 12,
+    "name": "Львівська Мануфактура кави",
+    "description": "A pet-friendly coffee shop with a cozy atmosphere and freshly brewed coffee.",
+    "rating": 5,
+    "image": "lviv-manufaktura.jpg"
+  },
+  {
+    "id": 13,
+    "name": "Довгі бурхливі оплески",
+    "description": "A pet-friendly bar with a lively atmosphere and refreshing drinks.",
+    "rating": 4.5,
+    "image": "burhlyvi-oplisky.jpg"
+  },
+  {
+    "id": 14,
+    "name": "Ресторан \"Valentino/Валентино\"",
+    "description": "A pet-friendly restaurant offering a romantic setting and delicious Italian cuisine.",
+    "rating": 4,
+    "image": "valentino.jpg"
+  },
+  {
+    "id": 15,
+    "name": "Ресторан \"Mrs.Greenwich\"",
+    "description": "A pet-friendly restaurant with a cozy ambiance and a variety of dishes.",
+    "rating": 4.5,
+    "image": "mrs-greenwich.jpg"
+  },
+  {
+    "id": 16,
+    "name": "Ресторан \"Na Virmenskiy\"",
+    "description": "A pet-friendly restaurant serving traditional Ukrainian cuisine in a historic setting.",
+    "rating": 4,
+    "image": "na-virmenskiy.jpg"
+  },
+  {
+    "id": 17,
+    "name": "Ресторан \"Papi\"",
+    "description": "A pet-friendly restaurant offering Mediterranean cuisine and a welcoming atmosphere.",
+    "rating": 4.5,
+    "image": "papi.jpg"
+  },
+  {
+    "id": 18,
+    "name": "Ресторан \"МОРЕ РИБИ\"",
+    "description": "A pet-friendly seafood restaurant with a wide selection of fresh fish dishes.",
+    "rating": 4,
+    "image": "more-ryby.jpg"
+  },
+  {
+    "id": 19,
+    "name": "Ресторан \"Epic Cheeseburger\"",
+    "description": "A pet-friendly restaurant known for its delicious cheeseburgers and casual atmosphere.",
+    "rating": 4.5,
+    "image": "epic-cheeseburger.jpg"
+  },
+  {
+    "id": 20,
+    "name": "Ресторан \"Cukor Black\"",
+    "description": "A pet-friendly restaurant offering gourmet cuisine and a stylish ambiance.",
+    "rating": 4,
+    "image": "cukor-black.jpg"
+  },
+  {
+    "id": 21,
+    "name": "Ресторан \"Teddy\"",
+    "description": "A pet-friendly restaurant with a cozy atmosphere and friendly staff.",
+    "rating": 4.5,
+    "image": "teddy.jpg"
+  }
+];
 
-  useEffect(() => {
-    setEstablishments(restaurantsData);
-  }, []);
+const EstablishmentCard = ({ establishment }) => (
+  <div className="relative mb-8 rounded-2xl p-6" style={{ backgroundColor: '#e89a6d', hover: { backgroundColor: '#7b492b' } }}>
+    <div className="flex">
+      <img src={establishment.image} className="w-64 h-64 mr-8 rounded-md" />
+      <div className="flex-1">
+        <h2 className="text-2xl font-semibold mb-4">{establishment.name}</h2>
+        <p className="text-[#572b11]">{establishment.description}</p>
+        <a href="/another-page" className="mt-auto">
+          <img src={arrowImage} alt="Arrow" className="w-12 h-12 absolute bottom-4 right-4" style={{ filter: 'brightness(150%)' }} />
+        </a>
+      </div>
+    </div>
+  </div>
+);
 
+
+
+const PetFriendlyPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-semibold mb-8">Pet-Friendly Establishments</h1>
-      <div className="flex flex-wrap">
+      <div className="grid grid-cols-2 gap-8">
         {establishments.map(establishment => (
-          <div key={establishment.id} className="w-full md:w-1/2 lg:w-1/2 pr-4 relative"> {/* Додано relative */}
-            <div className="border p-4 rounded-md mb-4">
-              <h2 className="text-lg font-semibold">{establishment.name}</h2>
-              <p className="text-gray-600">{establishment.description}</p>
-              <img src={`../assets/images/${establishment.image}`} alt={establishment.name} className="mt-4 rounded-md" />
-              
-              {/* Кнопка для переходу на сторінку з повним описом закладу */}
-              <Link to={`/establishment/${establishment.id}`} className="absolute top-0 right-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-                {/* Вставте вашу логіку тут */}
-                {nav ? <img src={pawIcon} alt="Open Menu" className="w-6 h-6"/> : <>&gt;</>}
-              </Link>
-            </div>
-          </div>
+          <EstablishmentCard key={establishment.id} establishment={establishment} />
         ))}
       </div>
     </div>
