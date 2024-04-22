c0f71a4dceb7cab5ad2323eee5650f8c32bdc3a1
Max Humeniuk <maxhumeniuk321@gmail.com>
2024-04-22 21:20:16 +0300

    fixed some animations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import 'tailwindcss/tailwind.css';
 
 export default function About(){
     return(
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-3xl font-bold">About page</h1>
+        <div className="flex justify-center items-center relative mb-8 rounded-2xl p-3 border-2 border-gray-400 bg-[#f7b95c] m-5">
+            <h1 className="text-3xl font-bold">Назва нашого проекту "Улюблені лапки".  Наша команда працює над  створеням сайту, у якому ви злегкістю знайдете pet-friendly місця. Наш сайт – ваш найкращий помічник у пошуку ідеальних місць для прогулянок і відпочинку з вашими улюбленцями. За допомогою нашої платформи ви зможете знайти найближчі парки, кафе, готелі та інші заклади, які ласкаво вітають дружніх тварин.</h1>
         </div>  
     )
 }
