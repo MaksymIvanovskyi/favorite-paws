3ad5c0d4382e204e63e37e7f223541825229248f
MaksymIvanovskyi <159261445+MaksymIvanovskyi@users.noreply.github.com>
2024-04-23 19:33:51 +0300

    animations and add map icon2

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,27 +1,28 @@
-import 'tailwindcss/tailwind.css';
 import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import MapIcon from "../assets/map-icon.png";
 
 export default function Locations() {
   const [places, setPlaces] = useState([]);
+  const [showMap, setShowMap] = useState(false);
+  const [isBouncing, setIsBouncing] = useState(false); // Додали новий стан для керування анімацією
 
   useEffect(() => {
     fetch('http://localhost:5000/api/places')
       .then(response => response.json())
       .then(data => setPlaces(data))
       .catch(error => console.error('Error fetching places:', error));
+
+    // Установка таймера для виконання bounce кожні 60 секунд
+    const intervalId = setInterval(() => {
+      setIsBouncing(true); // Позначаємо, що має відбутися анімація "bounce"
+      setTimeout(() => setIsBouncing(false), 1000); // Вимикаємо позначку після 1 секунди
+    }, 60000); // Кожні 60 секунд
+
+    // Прибирання таймера при виході з компонента
+    return () => clearInterval(intervalId);
   }, []);
-
-  const LocationsList = ({ locations }) => {
-    return (
-      <div>
-        {locations.map(location => <LocationItem key={location.id} location={location} />)}
-      </div>
-    );
-  };
-
-  const [showMap, setShowMap] = useState(false);
 
   const Map = ({ location }) => {
     const mapRef = useRef(null);
@@ -42,14 +43,14 @@ export default function Locations() {
     <div className="container mx-auto px-12 py-12">
       <h1> </h1>
       {places.map(place => (
-        <div key={place.id} className="relative mb-6 rounded-2xl p-2 border-2 border-[#f7b95c] bg-[#f7b95c] delay-100 duration-100 transform hover:scale-105 transition ease-linear">
-          <img src={place.imageUrl} alt={place.name} className="w-64 h-64 mr-8 rounded-md" />
+        <div key={place.id} className="relative mb-6 rounded-2xl p-2 border-2 border-[#ffcc7f] bg-[#ffcc7f] delay-100 duration-100 transform hover:scale-105 transition ease-linear">
+          <div className="flex items-center">
+            <img src={place.imageUrl} alt={place.name} className="w-64 h-64 mr-8 rounded-md" />
+            <img src={MapIcon} alt="map-icon" className={`w-14 h-14 cursor-pointer ${isBouncing ? 'animate-bounce' : ''}`} onClick={() => setShowMap(!showMap)} />
+          </div>
           <div>
-          <h3 className="text-2xl font-semibold mb-2 block border-b border-[#66421f]">{place.name}</h3>
+            <h3 className="text-2xl font-semibold mb-2 block border-b border-[#66421f]">{place.name}</h3>
             <p className="text-brown">{place.description}</p>
-            <button onClick={() => setShowMap(!showMap)}>
-              {showMap ? 'Hide' : 'Show'} Map
-            </button>
             {showMap && <Map location={place} />}
           </div>
         </div>
